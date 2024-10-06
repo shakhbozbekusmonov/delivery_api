260c117195d27e56b3a961e539b4ce1769e26d9e
shakhbozbekusmonov <shakhbozbek.usmonov@gmail.com>
2024-10-06 19:18:16 +0500

    feat: order and product table created

--- models.py
+++ models.py
@@ -13,3 +13,41 @@ class User(Base):
     password = Column(Text, nullable=False)
     is_staff = Column(Boolean, default=False)
     is_active = Column(Boolean, default=False)
+
+    orders = relationship("Order", back_populates="user")  # one-to-many relationship
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+
+class Order(Base):
+    ORDER_STATUS = (
+        ("PENDING", "pending"),
+        ("SHIPPED", "shipped"),
+        ("DELIVERED", "delivered"),
+    )
+
+    __tablename__ = "orders"
+
+    id = Column(Integer, primary_key=True)
+    quantity = Column(Integer, default=0)
+    status = Column(ChoiceType(ORDER_STATUS), default="PENDING")
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="orders")  # many-to-one relationship
+    product_id = Column(Integer, ForeignKey("product.id"))
+    product = relationship("Product", back_populates="orders")  # many-to-one relationship
+
+    def __repr__(self):
+        return f"<Order {self.id}>"
+
+
+class Product(Base):
+    __tablename__ = 'product'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    price = Column(Integer)
+
+    def __repr__(self):
+        return f"<Product {self.name}>"
